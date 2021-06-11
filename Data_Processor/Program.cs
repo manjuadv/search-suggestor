ec490da28ed9182d7a30bb830a31a3395fdd60d0
manjuadv <pathmakumaraad@gmail.com>
2021-06-12 01:02:41 +0530

    Data upload done

--- Data_Processor/Program.cs
+++ Data_Processor/Program.cs
@@ -1,4 +1,5 @@
-﻿using Nest;
+﻿using ElasticsearchConnector.Connectors.Aws;
+using Nest;
 using Newtonsoft.Json;
 using SmartApart.Core.Models;
 using SmartApart.Utils;
@@ -20,33 +21,78 @@ namespace Data_Processor
 
             ConnectionSettings settings = new ConnectionSettings(new Uri("http://localhost:9200/"));
             IElasticClient elasticClient = new ElasticClient(settings);
-            string indexName = "property1";
+            string indexName = "property4";
 
-            //IndexPropertyItem(elasticClient, indexName, propertyList[1]);
-            //SearchMatchPrase(elasticClient, "property1", "Abilene");
+            //CreateIndexAutoMap(elasticClient, indexName);
+
+            //IndexPropertyItem(elasticClient, indexName, propertyList[0]);
+            SearchMatchPrase(elasticClient, indexName, "Abilene");
             //SearchTerm(elasticClient, indexName, "Abilene");
-            //SearchMatchPrase(elasticClient, "property1", "Cur"); // No match
-            SearchMatchPrefixPhase(elasticClient, "property1", "Cur");
-            SearchMatchPrefixPhase(elasticClient, "property1", "Ranc");
+            //SearchMatchPrase(elasticClient, indexName, "Cur"); // No match
+            SearchMatchPrefixPhase(elasticClient, indexName, "cro");
+            //SearchMatchPrefixPhase(elasticClient, indexName, "Ranc");
 
-            Console.ReadKey();
+            //IndexPropertyItemBulkAll(elasticClient, indexName, propertyList, 10);
+
+
         }
         private static void CreateIndexAutoMap(IElasticClient elasticClient, string indexName)
         {
-            var createIndexResponse = elasticClient.Indices.CreateAsync(indexName, c => c
-            .Map<PropertyItem>(m => m.AutoMap()));
+            /*var createIndexResponse = elasticClient.Indices.CreateAsync(indexName, c => c
+            .Map<PropertyItem>(m => m.AutoMap()));*/
+            AwsElasticsearchSetupConnector setupConnector = new AwsElasticsearchSetupConnector(elasticClient);
+            setupConnector.CreateIndex(indexName);
         }
         private static void IndexPropertyItem(IElasticClient elasticClient, string indexName, PropertyItem proerpty)
         {
-            var response = elasticClient.Index(proerpty, i => i.Index(indexName));
-            Console.WriteLine(JsonConvert.SerializeObject(response.Result));
+            /*var response = elasticClient.Index(proerpty, i => i.Index(indexName));
+            Console.WriteLine(JsonConvert.SerializeObject(response.Result));*/
+            AwsElasticsearchSetupConnector setupConnector = new AwsElasticsearchSetupConnector(elasticClient);
+            setupConnector.IndexRecord(proerpty, indexName);
+        }
+        private static void IndexPropertyItemBulkAll(IElasticClient elasticClient, string indexName, List<PropertyItem> propertis, int itemsPerRequest)
+        {
+            /*var bulkAllObservable = elasticClient.BulkAll(propertis, b => b
+            .Index(indexName)
+            .BackOffTime("30s")
+            .BackOffRetries(2)
+            .RefreshOnCompleted()
+            .MaxDegreeOfParallelism(Environment.ProcessorCount)
+            .Size(itemsPerRequest))
+            .Wait(TimeSpan.FromMinutes(15), next =>
+            {
+                
+            });*/
+            AwsElasticsearchSetupConnector setupConnector = new AwsElasticsearchSetupConnector(elasticClient);
+            setupConnector.IndexRecordsBulkAll(propertis, indexName, itemsPerRequest);
+        }
+        private static void IndexPropertyItemBulk(IElasticClient elasticClient, string indexName, List<PropertyItem> propertis)
+        {
+            /*var response = elasticClient.Bulk(b => b.Index(indexName).IndexMany(propertis));
+            if (response.Errors)
+            {
+                foreach (var errorItem in response.ItemsWithErrors)
+                {
+                    Console.WriteLine(JsonConvert.SerializeObject(errorItem));
+                    Console.WriteLine(string.Format("Error happed while uploading property data bulk.", response.OriginalException));
+                }                
+            }
+            else
+            {
+                foreach (var responseItem in response.Items)
+                {
+                    Console.WriteLine(JsonConvert.SerializeObject(responseItem));
+                }                
+            }*/
+            AwsElasticsearchSetupConnector setupConnector = new AwsElasticsearchSetupConnector(elasticClient);
+            setupConnector.IndexRecordsBulk(propertis, indexName);
         }
 
         private static void SearchMatchPrase(IElasticClient elasticClient, string indexName, string key)
         {
             var results = elasticClient.Search<PropertyItem>(s => s
             .Index(indexName)
-            .Query(q => q.Match(m => m.Field(f => f.Name).Query(key))));
+            .Query(q => q.Match(m => m.Field(f => f.Name).Query(key)))) ;
 
             foreach (var result in results?.Documents)
             {
---
+++ ElasticsearchConnector/Connectors/Aws/AwsElasticsearchSetupConnector.cs
@@ -0,0 +1,66 @@
+﻿using Nest;
+using Newtonsoft.Json;
+using SmartApart.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElasticsearchConnector.Connectors.Aws
+{
+    public class AwsElasticsearchSetupConnector : ISetupConnector<PropertyItem>
+    {
+        private readonly IElasticClient elasticClient;
+
+        public AwsElasticsearchSetupConnector(IElasticClient elasticClient)
+        {
+            this.elasticClient = elasticClient;
+        }
+        public void CreateIndex(string indexName)
+        {
+            var createIndexResponse = elasticClient.Indices.CreateAsync(indexName, c => c
+           .Map<PropertyItem>(m => m.AutoMap()));
+        }
+
+        public void IndexRecord(PropertyItem proerpty, string indexName)
+        {
+            var response = elasticClient.Index(proerpty, i => i.Index(indexName));
+        }
+
+        public void IndexRecordsBulk(IEnumerable<PropertyItem> propertis, string indexName)
+        {
+            var response = elasticClient.Bulk(b => b.Index(indexName).IndexMany(propertis));
+            if (response.Errors)
+            {
+                foreach (var errorItem in response.ItemsWithErrors)
+                {
+                    Console.WriteLine(JsonConvert.SerializeObject(errorItem));
+                    Console.WriteLine(string.Format("Error happed while uploading property data bulk.", response.OriginalException));
+                }
+            }
+            else
+            {
+                foreach (var responseItem in response.Items)
+                {
+                    Console.WriteLine(JsonConvert.SerializeObject(responseItem));
+                }
+            }
+        }
+
+        public void IndexRecordsBulkAll(IEnumerable<PropertyItem> propertis, string indexName, int itemsPerRequest)
+        {
+            var bulkAllObservable = elasticClient.BulkAll(propertis, b => b
+            .Index(indexName)
+            .BackOffTime("30s")
+            .BackOffRetries(2)
+            .RefreshOnCompleted()
+            .MaxDegreeOfParallelism(Environment.ProcessorCount)
+            .Size(itemsPerRequest))
+            .Wait(TimeSpan.FromMinutes(15), next =>
+            {
+
+            });
+        }
+    }
+}
